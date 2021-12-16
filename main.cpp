39826a090efdc79513c934b213fa4ce9e6dacc10
Alberto Gálvez Gálvez <albertogg023@gmail.com>
2021-12-16 13:08:39 +0100

    Versión final del proyecto. Ahora el fichero de salida es la unión de los dos de entrada. Además solucionado algunos temas de compatibilidad con otros compiladores.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
-#include <assert.h>
-#include <iostream>  // variables cin y cout
+#include <cassert> // assert
+#include <cstdio>  // operaciones input/output, etc
+#include <cstdlib> // operaciones dinamicas, etc
+#include <iostream>  // cin y cout, strings, etc
 #include <sstream>  // iostream
-#include <fstream>  // lectura ficheros
+#include <fstream>  // lectura y escritura ficheros
 #include <list> // lista enlazada
+#include <cstring>  // funciones string
+#include <cmath>    // abs
 #include <climits> // MAX_INT y MIN_INT
 using namespace std;
+
+#define MODO_UN_LITERAL -1
+#define MODO_CONJUNCION 0
+#define MODO_DISYUNCION 1
+#define CADENA_INDICA_CONJUNCION "y"
+#define CADENA_INDICA_DISYUNCION "o"
 
 
 //////////////////////////////////////////////////////////////
@@ -13,16 +23,15 @@ using namespace std;
 
 typedef struct {
     string id;  // identificador de la regla
-    int modoAntecedentes;   // -1=unLiteral, 0=variosLiteralesConConjunciones, 1=variosLiteralesConDisyunciones
+    int modoAntecedentes;   // indica si un literal solo, o varios y si son separados por conjunciones o disyunciones
     string * antecedentes;  // array de antecedentes de la regla en memoria dinamica
     string consecuente; // consecuente de la regla
-    float factorCerteza;    // factor de certeza de la regla
+    double factorCerteza;    // factor de certeza de la regla
 } Conocimiento;
-
 
 typedef struct {
     string id;  // identificador del hecho
-    float factorCerteza;    // factor de certeza del hecho
+    double factorCerteza;    // factor de certeza del hecho
 } Hecho;
 
 
@@ -31,10 +40,10 @@ typedef struct {
 //////////////////////////////////////////////////////////////
 
 // LECTURA DE LA BASE DE CONOCIMIENTO DE UN FICHERO
-int lecturaBC(string file, list<Conocimiento> & listaConocimientos){
+void lecturaBC(string file, list<Conocimiento> & listaConocimientos){
 
-    ifstream BC(file); // para leer del fichero
-    string linea; string cadena; string basura; // para leer el contenido del fichero
+    ifstream BC(file.c_str()); // para leer del fichero
+    string linea; string cadena; string basura; char cBasura; // para leer el contenido del fichero
     list<string> listaAntecentes;   // para guardar provisionalmente los antecedentes de la regla actual
     int numReglas; BC >> numReglas;    // leemos el numero de reglas
     BC.ignore(1, ' ');  // saltamos a la siguiente linea
@@ -57,47 +66,46 @@ int lecturaBC(string file, list<Conocimiento> & listaConocimientos){
 
         // Lectura de los antecedentes de la regla
         listaAntecentes.clear();    // vaciamos la lista de antecedentes
-        int modoAntecedentes = -1;  // suponemos inicialmente que solo hay un literal
+        int modoAntecedentes = MODO_UN_LITERAL;  // suponemos inicialmente que solo hay un literal
         while(cadenas >> cadena && cadena != "Entonces" ){
-            if(cadena == "y")
-                modoAntecedentes = 0;
-            else if(cadena == "o")
-                modoAntecedentes = 1;
+            if(cadena == CADENA_INDICA_CONJUNCION)
+                modoAntecedentes = MODO_CONJUNCION;
+            else if(cadena == CADENA_INDICA_DISYUNCION)
+                modoAntecedentes = MODO_DISYUNCION;
             else
                 listaAntecentes.push_back(cadena);
         }
         conocimiento.modoAntecedentes = modoAntecedentes;
-        conocimiento.antecedentes = new string[listaAntecentes.size()+1];   // reservamos memoria para guardar los antecedentes y la marca de fin
+        conocimiento.antecedentes = new string[listaAntecentes.size()+1];   // reservamos memoria para guardar los antecedentes y la marca de fín
         int indAntecedentes = 0;
         list<string>::iterator it = listaAntecentes.begin();
         while(it != listaAntecentes.end()){ // insertamos los antecedentes de la regla
             conocimiento.antecedentes[indAntecedentes++] = *it;
             ++it;
-        } conocimiento.antecedentes[indAntecedentes] = "\0"; // insertamos marca de fin
+        } conocimiento.antecedentes[indAntecedentes] = "\0"; // insertamos marca de fín
 
         // Lectura consecuente
         cadenas >> cadena;
         conocimiento.consecuente = cadena.substr(0, cadena.size()-1);
 
         // Lectura FC
-        cadenas >> cadena;
-        conocimiento.factorCerteza = stof(cadena.substr(3));
-
-        listaConocimientos.push_back(conocimiento); // insertamos el conocimiento en la coleccion
+        cadenas >> cBasura;
+        cadenas >> cBasura;
+        cadenas >> cBasura;
+        cadenas >> conocimiento.factorCerteza;
+        listaConocimientos.push_back(conocimiento); // insertamos el conocimiento en la colección
 
     }
 
-    BC.close();
-
-    return 0;
+    BC.close(); // cerramos el fichero
 
 }
 
 // LECTURA DE LA BASE DE HECHOS DE UN FICHERO
-int lecturaBH(string file, list<Hecho> & listaHechos, string & objetivo){
+void lecturaBH(string file, list<Hecho> & listaHechos, string & objetivo){
 
-    ifstream BH(file); // para leer del fichero
-    string linea; string cadena; string basura; // para lectura del contenido del fichero
+    ifstream BH(file.c_str()); // para leer del fichero
+    string linea; string cadena; string basura; char cBasura; // para lectura del contenido del fichero
 
     int numHechos; BH >> numHechos;    // leemos el numero de reglas
     BH.ignore(1, ' ');  // saltamos a la siguiente linea
@@ -115,10 +123,12 @@ int lecturaBH(string file, list<Hecho> & listaHechos, string & objetivo){
         hecho.id = cadena.substr(0,cadena.size()-1);
 
         // Lectura el factor de certeza del hecho
-        cadenas >> cadena;
-        hecho.factorCerteza = stof(cadena.substr(3));
+        cadenas >> cBasura;
+        cadenas >> cBasura;
+        cadenas >> cBasura;
+        cadenas >> hecho.factorCerteza;
 
-        listaHechos.push_back(hecho);   // insertamos el hecho en la coleccion
+        listaHechos.push_back(hecho);   // insertamos el hecho en la colección
 
     }
 
@@ -128,9 +138,7 @@ int lecturaBH(string file, list<Hecho> & listaHechos, string & objetivo){
     // Obtenemos el objetivo
     BH >> objetivo;
 
-    BH.close();
-
-    return 0;
+    BH.close(); // cerramos el fichero
 
 }
 
@@ -139,26 +147,26 @@ int lecturaBH(string file, list<Hecho> & listaHechos, string & objetivo){
 /////////          FUNCIONES AUXILIARES              /////////
 //////////////////////////////////////////////////////////////
 
-// LIBERAR TODA LA MEMORIA DINAMICA RESERVADA DURANTE LA EJECUCION DEL PROGRAMA
+// LIBERAR TODA LA MEMORIA DINÁMICA RESERVADA DURANTE LA EJECUCIÓN DEL PROGRAMA
 void liberarMemoriaDinamica(list<Conocimiento> & BC){
     list<Conocimiento>::iterator it = BC.begin();
-    while(it != BC.end()){ // insertamos los antecedentes de la regla
+    while(it != BC.end()){
         delete[] (*it).antecedentes;
         ++it;
     }
 }
 
-// DEVUELVE SI LA COLECCION PASADA COMO PARAMETRO ES VACIA O NO
+// DEVUELVE SI LA COLECCION PASADA COMO PARÁMETRO ES VACIA O NO
 bool esVacia(list<Conocimiento> & coleccion){
     return coleccion.empty();
 }
 
-// DEVUELVE SI LA COLECCION PASADA COMO PARAMETRO ES VACIA O NO
+// DEVUELVE SI LA COLECCION PASADA COMO PARÁMETRO ES VACIA O NO
 bool esVacia(list<string> & coleccion){
     return coleccion.empty();
 }
 
-// DEVUELVE UNA LISTA DE STRINGS A PARTIR DE UN ARRAY QUE TENGA MARCA DE FIN
+// DEVUELVE UNA LISTA DE STRINGS A PARTIR DE UN ARRAY QUE TENGA MARCA DE FÍN
 list<string> arrayStringsDinamicoToList(string * arrayStrings){
     list<string> listaStrings;
     int i = 0;
@@ -177,7 +185,7 @@ bool contenida(string meta, list<Hecho> & BH){
             return true;    // lo hemos encontrado
         ++it;
     }
-    return false;   // hemos acabado la busqueda y no lo hemos encontrado
+    return false;   // hemos acabado la búsqueda y no lo hemos encontrado
 }
 
 // OBTIENE LAS REGLAS DE LA BC CUYOS CONSECUENTES SEAN EL OBJETIVO
@@ -194,7 +202,7 @@ list<Conocimiento> equiparar(list<Conocimiento> & BC, string objetivo){
 
 // OBTIENE UNA REGLA DEL CJTO CONFLICTO Y LA ELIMINA DE ESTE
 Conocimiento resolver(list<Conocimiento> & cjtoConflicto){
-    assert(!cjtoConflicto.empty()); // comprobamos que efectivamente no es vacio el cjto
+    assert(!cjtoConflicto.empty()); // comprobamos que efectivamente no es vacío el cjto
     Conocimiento conocimiento = cjtoConflicto.front();  // devolvemos el primer elemento del cjto
     cjtoConflicto.pop_front();  // lo eliminamos del cjto
     return conocimiento;
@@ -202,7 +210,7 @@ Conocimiento resolver(list<Conocimiento> & cjtoConflicto){
 
 // OBTIENE UNA META DEL CJTO NUEVASMETAS Y LA ELIMINA DE ESTE
 string popMeta(list<string> & metas){
-    assert(!metas.empty()); // comprobamos que efectivamente no es vacio el cjto
+    assert(!metas.empty()); // comprobamos que efectivamente no es vacío el cjto
     string meta = metas.front();  // devolvemos el primer elemento del cjto
     metas.pop_front();  // lo eliminamos del cjto
     return meta;
@@ -228,13 +236,14 @@ double getFactorCerteza(list<Hecho> & BH, string id){
 //////////////////////////////////////////////////////////////
 /////////  CASOS PARA CALCULAR FACTORES DE CERTEZA   /////////
 //////////////////////////////////////////////////////////////
+
 double aplicaCaso3(Conocimiento & reglaAplicada, list<Hecho> & BH, ofstream & out){
     double fcCalculado;
     double fcAntecedente = getFactorCerteza(BH, reglaAplicada.antecedentes[0]);
     out << "INICIO CASO 3" << endl;
     out << "Factor de certeza de la regla es " << reglaAplicada.factorCerteza << endl;
     out << "Factor de certeza del antecedente " << reglaAplicada.antecedentes[0] << " es: " << fcAntecedente << endl;
-    if(fcAntecedente > 0)    // el factor de certeza del antecedene es mayor que 0
+    if(fcAntecedente > 0)    // el factor de certeza del antecedente es mayor que 0
         fcCalculado = reglaAplicada.factorCerteza*fcAntecedente;
     else    // en caso contrario
         fcCalculado = 0;
@@ -248,7 +257,7 @@ double aplicaCaso3TrasCaso1(Conocimiento & reglaAplicada, double fcAntecedente, 
     out << "INICIO CASO 3" << endl;
     out << "Factor de certeza de la regla es " << reglaAplicada.factorCerteza << endl;
     out << "Factor de certeza del antecedente " << reglaAplicada.antecedentes[0] << " es: " << fcAntecedente << endl;
-    if(fcAntecedente > 0)    // el factor de certeza del antecedene es mayor que 0
+    if(fcAntecedente > 0)    // el factor de certeza del antecedente es mayor que 0
         fcCalculado = reglaAplicada.factorCerteza*fcAntecedente;
     else    // en caso contrario
         fcCalculado = 0;
@@ -304,10 +313,10 @@ double aplicaCaso2(double * fcReglas, unsigned int numReglasAplicadas, ofstream 
         else if(fcCalculada <= 0 && fcReglas[i] <= 0)
             fcCalculada = fcCalculada + fcReglas[i]*(1+fcCalculada);
         else{
-            if(abs(fcCalculada) < abs(fcReglas[i]))
-                fcCalculada = (fcCalculada + fcReglas[i]) / (1 - abs(fcCalculada));
+            if(fabs(fcCalculada) < fabs(fcReglas[i]))
+                fcCalculada = (fcCalculada + fcReglas[i]) / (1 - fabs(fcCalculada));
             else
-                fcCalculada = (fcCalculada + fcReglas[i]) / (1 - abs(fcReglas[i]));
+                fcCalculada = (fcCalculada + fcReglas[i]) / (1 - fabs(fcReglas[i]));
         }
     }
     out << "Factor de certeza calculado: " << fcCalculada << endl << "FIN CASO 2" << endl;
@@ -331,11 +340,11 @@ double calcularFactorCerteza(string objetivo, list<Conocimiento> & cjtoReglasApl
         reglaAplicada = resolver(cjtoReglasAplicadas);  // obtenemos y eliminamos una regla del cjto ReglasAplicadas
         out << "Para calcular " << reglaAplicada.consecuente << " con " << reglaAplicada.id << endl;
 
-        if(reglaAplicada.modoAntecedentes == -1){    // CASO 3: la regla tiene un solo antecedente
+        if(reglaAplicada.modoAntecedentes == MODO_UN_LITERAL){    // CASO 3: la regla tiene un solo antecedente
             fcReglas[i] = aplicaCaso3(reglaAplicada, BH, out);
         }
         else{   // CASO 1: la regla tiene más de un antecedente
-            if(reglaAplicada.modoAntecedentes == 0)   //  conjunciones
+            if(reglaAplicada.modoAntecedentes == MODO_CONJUNCION)   //  conjunciones
                 fcAntecedente = aplicaCaso1Conjunciones(reglaAplicada, BH, out); // nos quedamos con el menor fc de sus antecedentes
             else   // disyunciones
                 fcAntecedente = aplicaCaso1Disyunciones(reglaAplicada, BH, out); // nos quedamos con el menor fc de sus antecedentes
@@ -346,7 +355,7 @@ double calcularFactorCerteza(string objetivo, list<Conocimiento> & cjtoReglasApl
 
     // Vemos que factor de certeza debemos de devolver
     if(numReglasAplicadas > 1){ // CASO 2: se ha llegado al objetivo a través de varias ramas
-         out << "Para cara calcular " << reglaAplicada.consecuente << " acumulando evidencia" << endl;
+         out << "Para calcular " << reglaAplicada.consecuente << " acumulando evidencia" << endl;
         fcAdquirida = aplicaCaso2(fcReglas, numReglasAplicadas, out);
     }else   // se ha llegado al objetivo a través de una rama solo
         fcAdquirida = fcReglas[0];
@@ -366,7 +375,7 @@ bool verificar(list<Conocimiento> & BC, list<Hecho> & BH, string objetivo, ofstr
     list<string> nuevasMetas;
     bool verificado = false;
 
-    if( contenida(objetivo, BH) )
+    if(contenida(objetivo, BH))
         return true;
 
     list<Conocimiento> cjtoConflicto = equiparar(BC, objetivo);
@@ -374,16 +383,16 @@ bool verificar(list<Conocimiento> & BC, list<Hecho> & BH, string objetivo, ofstr
 
     while(!esVacia(cjtoConflicto)){
 
-        r = resolver(cjtoConflicto); // devuelve una regla de cjtoConflicto y la elimina de el
+        r = resolver(cjtoConflicto); // devuelve una regla de cjtoConflicto y la elimina de él
         nuevasMetas = arrayStringsDinamicoToList(r.antecedentes);   // obtenemos los antecedentes de la regla
         verificado = true;
 
         while(!esVacia(nuevasMetas) && verificado){
-            nuevaMeta = popMeta(nuevasMetas);
+            nuevaMeta = popMeta(nuevasMetas); // devuelve una meta de nuevasMetas y la elimina de él
             verificado = verificar(BC, BH, nuevaMeta, out);
         }
 
-        if(verificado && esVacia(cjtoConflicto)){
+        if(verificado && esVacia(cjtoConflicto)){   // si se ha podido verificar y lo hemos hecho por todas sus ramas
             Hecho meta; meta.id = objetivo;
             meta.factorCerteza = calcularFactorCerteza(objetivo, reglasAplicadas, BH, out);
             anadirHecho(BH, meta);
@@ -400,31 +409,40 @@ bool verificar(list<Conocimiento> & BC, list<Hecho> & BH, string objetivo, ofstr
 ////////////        PROGRAMA PRINCIPAL        ////////////////
 //////////////////////////////////////////////////////////////
 
+int main (int argc, char ** argv){
 
-int main (int argc, char * argv[]){
-
+    // Leemos la base de conocimiento
     list<Conocimiento> BC;
-    lecturaBC("BC-ejercicio4.txt", BC);
+    lecturaBC(argv[1], BC);
 
+    // Leemos la base de hechos
     string objetivo;
     list<Hecho> BH;
-    lecturaBH("BH-ejercicio4.txt", BH, objetivo);
-    ofstream out("salida-ejercicio4.txt");
+    lecturaBH(argv[2], BH, objetivo);
 
+    // Creamos un fichero de salida para redirigir la salida
+    string nombreBC(argv[1]);
+    unsigned int posPunto = nombreBC.rfind(".");
+    string nombreBH(argv[2]);
+    posPunto = nombreBH.rfind(".");
+    string nombreFicheroSalida = "salida-" + nombreBC.substr(0, posPunto) + "-" + nombreBH.substr(0, posPunto) + ".txt";
+    ofstream out(nombreFicheroSalida.c_str(), ios::out);
+
+    // Mostramos información al usuario sobre los ficheros usados
+    out << "Base de conocimiento usada: " << nombreBC << endl  << endl;
+    out << "Base de hechos utilizada: " << nombreBH << endl << endl;
+    out << "Objetivo a calcular: " << objetivo << endl;
+    out << endl << endl << endl;
+    out << "--------------------------------------------------------------------------------------------";
+    out << endl << endl << endl << endl;
+
+    // Calculamos la red de inferencia
     if(verificar(BC, BH, objetivo, out))
         out << "EXITO, FACTOR DE CERTEZA DE " << objetivo << " es " << getFactorCerteza(BH, objetivo) << endl;
     else
         out << "FRACASO" << endl;
 
-    out << endl << endl << endl;
-    out << "--------------------------------------------------------------------------------------------";
-    out << endl << endl << endl << endl << "FACTORES DE CERTEZA DE LOS HECHOS:" << endl;
-    list<Hecho>::iterator it = BH.begin();
-    while(it != BH.end()){ // recorremos BH
-        out << "\t" << (*it).id << ": " << (*it).factorCerteza << endl;
-        ++it;
-    }
+    liberarMemoriaDinamica(BC); // liberamos la memoria dinámica reservada
+    out.close();    // cerramos el fichero utilizado para redirigir la salida
 
-    liberarMemoriaDinamica(BC);
-    out.close();
 }
